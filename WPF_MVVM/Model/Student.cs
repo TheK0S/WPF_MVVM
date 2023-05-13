8472b2abf4b76c3481536262c63edfc976cd23ee
Kos <turchakkonstantin@gmail.com>
2023-05-13 18:32:47 +0300

    Добавьте файлы проекта.

---
+++ WPF_MVVM/Model/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM.Model
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double GPA { get; set; }
+
+        public Student(int id, string firstName, string lastName, int age, double gPA)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            GPA = gPA;
+        }
+    }
+}
---
+++ WPF_MVVM/ViewModel/StudentViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_MVVM.Model;
+
+namespace WPF_MVVM.ViewModel
+{
+    internal class StudentViewModel : INotifyPropertyChanged
+    {
+        List<Student> students;
+
+        public StudentViewModel()
+        {
+            students = new List<Student>
+            {
+                new Student(1, "Василий", "Пупкин", 18, 10.1),
+                new Student(1, "Петр", "Бубкин", 17, 10.3),
+                new Student(1, "Александр", "Мамонов", 19, 11),
+                new Student(1, "Олег", "Скрипка", 20, 9.4),
+                new Student(1, "Сергей", "Бобров", 18, 8.7),
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanget([CallerMemberName] string prop = "")
+        {
+            if(PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
